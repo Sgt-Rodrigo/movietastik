51387e825b43a0df5a952103b25aeef824d44991
Sgt-Rodrigo <rod_raven@hotmail.com>
2024-01-23 16:01:29 -0300

    applies main requested logic and finishes mobile layout

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,6 @@
+
 class Activity {
-    contructor(id, title, description, imgUrl){
+    constructor(id, title, description, imgUrl){
         this.id = id;
         this.title = title,
         this.description = description,
@@ -19,27 +20,107 @@ class Repository {
     }
 
     createActivity(title, description, imgUrl){
-        const newActividad = new Activity(this.contador, title, description, imgUrl);
-        this.activities.push(newActividad);
+        const newActivity = new Activity(this.contador, title, description, imgUrl);
+        this.activities.push(newActivity);
         this.contador++
         
     }
 
     deleteActivity (id) {
-        this.activities = this.activities.filter(activity => this.activity.id !== id );
+        this.activities = this.activities.filter(activity => activity.id !== id);
         
     }
+
+    
 }
 
-const repositorio = new Repository();
-console.log(repositorio);
-repositorio.createActivity('saltar la cuerda', 'descripcion', 'url');
-console.log(repositorio);
-repositorio.createActivity('correr', 'descripcion', 'url');
-console.log(repositorio);
-repositorio.createActivity('bailar', 'descripcion', 'url');
-console.log(repositorio);
+const newRepo = new Repository();
 
-repositorio.deleteActivity(3);
-console.log(repositorio);
 
+function createCard({id, title, description, imgUrl}) {
+    const cardContainer = document.createElement('div');
+    
+    const cardTitle = document.createElement('h3');    
+    const cardDescription = document.createElement('p');    
+    const imageContainer = document.createElement('div');
+    const cardImage = document.createElement('img');
+    const idHidden = document.createElement('input');
+    idHidden.type = 'hidden';
+    idHidden.value = '';
+    
+    //?? class asignment
+    //*cardContainer
+    cardContainer.classList.toggle('card');
+    //*cardTitle
+    cardTitle.textContent = title;
+    cardTitle.classList.toggle('card__title');
+    //*cardDescription
+    cardDescription.textContent = description;
+    cardDescription.classList.toggle('card__description');
+    //*imageContainer
+    imageContainer.classList.toggle('image__container');
+    //*cardImage
+    cardImage.src = imgUrl;
+    cardImage.alt = `${title} related image`;
+    cardImage.classList.toggle('card__image');
+    //*setId
+    idHidden.value = id;
+    
+    //*appending image to imageContainer
+    imageContainer.append(cardImage);
+
+    //*appending all in the cardContainer
+    cardContainer.append(cardTitle, imageContainer, cardDescription);
+
+    //*appending all into the cards container
+    // cards.append(cardTitle, cardImage, cardDescription, idHidden);
+
+    return cardContainer
+
+}
+
+
+
+function updateCards() {
+    const cards = document.querySelector('.cards');
+
+    cards.innerHTML = '';
+    //*returns a new array with card objects
+    const allCards = newRepo.getAllActivities().map(activity => createCard({id:activity.id, title:activity.title, description:activity.description, imgUrl:activity.imgUrl}));
+
+    //*appends all card nodes in the card container
+    allCards.forEach(card => {
+        cards.append(card)
+    })
+
+}
+
+
+function renderCards (){
+    const inputTitle = document.querySelector('#movie-title');
+    const inputDescription = document.querySelector('#movie-description');
+    const inputImage = document.querySelector('#poster')
+
+    //*usr Input
+    const usrTitle = inputTitle.value;
+    const usrDescription = inputDescription.value;
+    const usrImage = inputImage.value;
+
+    //*verify non-empty string
+    if(!usrTitle | !usrDescription | ! usrImage) {
+        alert('Complete all three fields mate');
+        resetInputs();
+        return
+    }
+
+    newRepo.createActivity(usrTitle, usrDescription, usrImage);
+    updateCards();
+}
+
+//* handler button
+
+const addButton = document.querySelector('.add-btn');
+
+addButton.addEventListener('click', (e)=>{
+    renderCards();
+})
